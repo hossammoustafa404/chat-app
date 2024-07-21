91389cdd93f3272871be825680556cdc235cbd39
hossammoustafa404 <hossammoustafa404@gmail.com>
2024-07-21 16:06:06 +0300

    Setup nx workspace & chat-app-frontend project

---
+++ apps/chat-app-frontend/app/page.tsx
@@ -0,0 +1,1 @@
+export { default } from '@/fsd-pages/home';
---
+++ apps/chat-app-frontend/src/fsd-pages/home/ui/Home.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/shared/ui/button';
+
+const Home = () => {
+  return (
+    <section>
+      <h1>Hello World!</h1>
+      <Button>Click Here</Button>
+    </section>
+  );
+};
+
+export default Home;
